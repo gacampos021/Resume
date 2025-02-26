406df17b242aa991abaa7bf77c3c771b96d5e9c5
Gabriel <gacampos021@gmail.com>
2025-02-26 17:18:10 -0300

    mobile version with burguer menu

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-colorDark={
-    'htmlColor': 'antiquewhite', //same for font and incon
-    'headerBG': 'rgba(22,22,22)',
-    'mainBG': 'rgb(43, 43, 43)',
-    'secBG': 'rgba(33,33,33)',
-    'filterButton': 'rgba(255, 255, 255, 0.2)'
-}
-
 let html = document.getElementById('html');
-let rightCard = document.getElementById('right')
+let rightCard = document.getElementById('right');
 
 function mobileCard(){
     rightCard.classList.toggle('card');
+}
+
+function burguerMenu(){
+    
+    let menu = document.getElementById('menu-sticky');
+    console.log(menu);
+    
+    menu.classList.toggle('hidden');
 }
 
 function darkMode(){
